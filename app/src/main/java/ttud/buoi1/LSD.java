5d2b92dfd18c692c29a505a00645f073f1047918
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2025-08-22 10:39:20 +0700

    init

---
+++ app/src/main/java/ttud/buoi1/LSD.java
@@ -0,0 +1,7 @@
+package ttud.buoi1;
+
+public class LSD {
+    // public static void sort(String[] a, int w) {
+
+    // } 
+}
---
+++ app/src/main/java/ttud/buoi2/Student.java
@@ -0,0 +1,5 @@
+package ttud.buoi2;
+
+public record Student(int id, String lastname, String name) {
+    
+}
